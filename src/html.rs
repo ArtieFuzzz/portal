9e109993679f6a5355b19c72ac39fb09e1af5683
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2023-08-15 08:15:30 +1000

    chore(deps): Update deps

--- src/html.rs
+++ src/html.rs
@@ -3,4 +3,5 @@ pub static HOME_PAGE: &str = "<!DOCTYPE html>
   <h1>OwO</h1>
   <p>Welcome to portal, A URL shortener made by me, currently it's being ran on Cloudflare Workers!</p>
   <p>This is the home page! It's pretty boring but maybe one day I'll spice things up :3</p>
-</body>";
+</body>
+</html>";
